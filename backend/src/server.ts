f310ac258c5054bf30a434cfd724a1176c557ad7
Robert Dominato <36608336+robertd77@users.noreply.github.com>
2026-01-30 11:36:35 -0500

    Update CORS settings to allow multiple origins

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -12,7 +12,10 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: [
+            "http://localhost:3000",
+            "https://task-tracker-psi-eight-70.vercel.app"
+            ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   }),
